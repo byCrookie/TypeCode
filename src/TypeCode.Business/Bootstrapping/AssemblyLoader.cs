d2a7b194b7f4f39da166afb7f5ebb3202e80feff
byCrookie <maeni.h01@hotmail.com>
2022-03-16 19:34:36 +0100

    Cache Using Hash

--- src/TypeCode.Business/Bootstrapping/AssemblyLoader.cs
+++ src/TypeCode.Business/Bootstrapping/AssemblyLoader.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 using Serilog;
 
 namespace TypeCode.Business.Bootstrapping;
@@ -11,10 +13,9 @@ internal class AssemblyLoader : IAssemblyLoader
         Directory.CreateDirectory(cacheDirectory);
 
         var fileName = Path.GetFileNameWithoutExtension(path);
-        var directoryPath = Path.GetDirectoryName(path) ?? string.Empty;
+        var directoryPath =  Path.GetDirectoryName(path) ?? string.Empty;
 
-        var cacheDirectoryPath = Path.Combine(cacheDirectory,
-            directoryPath.Replace(":", "").Replace("\\", "").Replace(".", ""));
+        var cacheDirectoryPath = Path.Combine(cacheDirectory, GetHashString(directoryPath));
         Directory.CreateDirectory(cacheDirectoryPath);
 
         var cachedAssembly = Path.Combine(cacheDirectoryPath, $"{fileName}.dll");
@@ -39,5 +40,23 @@ internal class AssemblyLoader : IAssemblyLoader
     private static bool AssemblyIsNewer(string path, string cachedAssembly)
     {
         return File.GetLastWriteTime(path) > File.GetLastWriteTime(cachedAssembly);
+    }
+
+    private static string GetHashString(string inputString)
+    {
+        var sb = new StringBuilder();
+        foreach (var b in GetHash(inputString))
+        {
+            sb.Append(b.ToString("X2"));
+        }
+        return sb.ToString();
+    }
+    
+    private static IEnumerable<byte> GetHash(string inputString)
+    {
+        using (HashAlgorithm algorithm = SHA1.Create())
+        {
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
     }
 }
