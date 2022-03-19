73df5c711ec94a48c62fe1f0aade3620a51c4889
byCrookie <maeni.h01@hotmail.com>
2022-03-19 16:17:41 +0100

    Remove Cache

--- src/TypeCode.Business/Configuration/AssemblyFileLoader.cs
+++ src/TypeCode.Business/Configuration/AssemblyFileLoader.cs
@@ -1,7 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.Loader;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace TypeCode.Business.Configuration;
 
@@ -9,54 +6,14 @@ public class AssemblyFileLoader : IAssemblyFileLoader
 {
     public Task<Assembly> LoadAsync(string path)
     {
-        const string cacheDirectory = "cache";
-        Directory.CreateDirectory(cacheDirectory);
-
-        var fileName = Path.GetFileNameWithoutExtension(path);
-        var directoryPath = Path.GetDirectoryName(path) ?? string.Empty;
-
-        var cacheDirectoryPath = Path.Combine(cacheDirectory, GetHashString(directoryPath));
-        Directory.CreateDirectory(cacheDirectoryPath);
-
-        var cachedAssembly = Path.Combine(cacheDirectoryPath, $"{fileName}.dll");
-
-        if (!File.Exists(cachedAssembly) || AssemblyIsNewer(path, cachedAssembly))
-        {
-            File.Copy(path, cachedAssembly, true);
-        }
-
-        return LoadFromPathAsync(cachedAssembly);
+        return LoadFromPathAsync(path);
     }
 
     private static async Task<Assembly> LoadFromPathAsync(string path)
     {
         await using (var fs = new FileStream(Path.GetFullPath(path), FileMode.Open))
         {
-            return new AssemblyLoadContext(Guid.NewGuid().ToString("N")).LoadFromStream(fs);
-        }
-    }
-
-    private static bool AssemblyIsNewer(string path, string cachedAssembly)
-    {
-        return File.GetLastWriteTime(path) > File.GetLastWriteTime(cachedAssembly);
-    }
-
-    private static string GetHashString(string inputString)
-    {
-        var sb = new StringBuilder();
-        foreach (var b in GetHash(inputString))
-        {
-            sb.Append(b.ToString("X2"));
-        }
-
-        return sb.ToString();
-    }
-
-    private static IEnumerable<byte> GetHash(string inputString)
-    {
-        using (HashAlgorithm algorithm = SHA1.Create())
-        {
-            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+            return new CustomAssemblyLoadContext().LoadFromStream(fs);
         }
     }
 }
---
+++ src/TypeCode.Business/Configuration/CustomAssemblyLoadContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Loader;
+
+namespace TypeCode.Business.Configuration;
+
+public class CustomAssemblyLoadContext : AssemblyLoadContext
+{
+    public CustomAssemblyLoadContext() : base(Guid.NewGuid().ToString("N"))
+    {
+        
+    }
+}
