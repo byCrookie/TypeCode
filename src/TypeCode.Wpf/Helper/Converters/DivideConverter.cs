d4706693ff22617cf019c5e2dba8712609f5220c
byCrookie <maeni.h01@hotmail.com>
2022-03-30 19:45:30 +0200

    Better Home

---
+++ src/TypeCode.Wpf/Helper/Converters/DivideConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TypeCode.Wpf.Helper.Converters;
+
+public class DivideConverter : IValueConverter
+{
+    public double DivideValue { get; set; }
+    
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double doubleValue)
+        {
+            return doubleValue / DivideValue;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
