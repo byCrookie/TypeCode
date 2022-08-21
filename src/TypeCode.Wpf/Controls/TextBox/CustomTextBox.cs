be4b9beda38e577feb6f2d4583a0760f20f1b4d1
byCrookie <maeni.h01@hotmail.com>
2022-08-21 18:33:59 +0200

    validation not working

---
+++ src/TypeCode.Wpf/Controls/TextBox/CustomTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TypeCode.Wpf.Controls;
+
+public class CustomTextBox : TextBox
+{
+    public static readonly DependencyProperty UseRegexProperty =
+        DependencyProperty.Register(
+            name: nameof(UseRegex),
+            propertyType: typeof(bool),
+            ownerType: typeof(CustomTextBox),
+            typeMetadata: new FrameworkPropertyMetadata(false)
+        );
+
+    public bool UseRegex
+    {
+        get => (bool)GetValue(UseRegexProperty);
+        set => SetValue(UseRegexProperty, value);
+    }
+    
+    public static readonly DependencyProperty ShowRegexProperty =
+        DependencyProperty.Register(
+            name: nameof(ShowRegex),
+            propertyType: typeof(bool),
+            ownerType: typeof(CustomTextBox),
+            typeMetadata: new FrameworkPropertyMetadata(false)
+        );
+    
+    public bool ShowRegex
+    {
+        get => (bool)GetValue(ShowRegexProperty);
+        set => SetValue(ShowRegexProperty, value);
+    }
+}
