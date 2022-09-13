218a62b31c6b1e2fa1dd62841aa4f3c6c986bf43
byCrookie <maeni.h01@hotmail.com>
2022-09-13 20:04:55 +0200

    enable disable textbox clear

--- src/TypeCode.Wpf/Helper/Triggers/PropertySetAction.cs
+++ src/TypeCode.Wpf/Helper/Triggers/PropertySetAction.cs
@@ -35,9 +35,105 @@ public sealed class SetPropertyActionTrigger : TriggerAction<FrameworkElement>
     public static readonly DependencyProperty TargetObjectProperty
         = DependencyProperty.Register(nameof(TargetObject), typeof(object),
             typeof(SetPropertyActionTrigger));
+    
+    public string? IfTargetPropertyIsNullOrEmptyPropertyName
+    {
+        get => (string?)GetValue(IfTargetPropertyIsNullOrEmptyPropertyNameProperty);
+        set => SetValue(IfTargetPropertyIsNullOrEmptyPropertyNameProperty, value);
+    }
+
+    public static readonly DependencyProperty IfTargetPropertyIsNullOrEmptyPropertyNameProperty
+        = DependencyProperty.Register(nameof(IfTargetPropertyIsNullOrEmptyPropertyName), typeof(string),
+            typeof(SetPropertyActionTrigger));
+    
+    public string? IfTargetPropertyIsNotNullOrEmptyPropertyName
+    {
+        get => (string?)GetValue(IfTargetPropertyIsNotNullOrEmptyPropertyNameProperty);
+        set => SetValue(IfTargetPropertyIsNotNullOrEmptyPropertyNameProperty, value);
+    }
+
+    public static readonly DependencyProperty IfTargetPropertyIsNotNullOrEmptyPropertyNameProperty
+        = DependencyProperty.Register(nameof(IfTargetPropertyIsNotNullOrEmptyPropertyName), typeof(string),
+            typeof(SetPropertyActionTrigger));
+    
+    public string? IfPropertyIsNullOrEmptyPropertyName
+    {
+        get => (string?)GetValue(IfPropertyIsNullOrEmptyPropertyNameProperty);
+        set => SetValue(IfPropertyIsNullOrEmptyPropertyNameProperty, value);
+    }
+
+    public static readonly DependencyProperty IfPropertyIsNullOrEmptyPropertyNameProperty
+        = DependencyProperty.Register(nameof(IfPropertyIsNullOrEmptyPropertyName), typeof(string),
+            typeof(SetPropertyActionTrigger));
+    
+    public string? IfPropertyIsNotNullOrEmptyPropertyName
+    {
+        get => (string?)GetValue(IfPropertyIsNotNullOrEmptyPropertyNameProperty);
+        set => SetValue(IfPropertyIsNotNullOrEmptyPropertyNameProperty, value);
+    }
+
+    public static readonly DependencyProperty IfPropertyIsNotNullOrEmptyPropertyNameProperty
+        = DependencyProperty.Register(nameof(IfPropertyIsNotNullOrEmptyPropertyName), typeof(string),
+            typeof(SetPropertyActionTrigger));
 
     protected override void Invoke(object parameter)
     {
+        if (IfTargetPropertyIsNullOrEmptyPropertyName is not null)
+        {
+            var nullOrEmptyPropertyInfo = TargetObject.GetType().GetProperty(
+                IfTargetPropertyIsNullOrEmptyPropertyName,
+                BindingFlags.Instance | BindingFlags.Public
+                                      | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+
+            var value = nullOrEmptyPropertyInfo?.GetValue(TargetObject);
+            if (value is not string stringValue || !string.IsNullOrEmpty(stringValue))
+            {
+                return;
+            }
+        }
+        
+        if (IfTargetPropertyIsNotNullOrEmptyPropertyName is not null)
+        {
+            var notNullOrEmptyPropertyInfo = TargetObject.GetType().GetProperty(
+                IfTargetPropertyIsNotNullOrEmptyPropertyName,
+                BindingFlags.Instance | BindingFlags.Public
+                                      | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+
+            var value = notNullOrEmptyPropertyInfo?.GetValue(TargetObject);
+            if (value is not string stringValue || string.IsNullOrEmpty(stringValue))
+            {
+                return;
+            }
+        }
+        
+        if (IfPropertyIsNullOrEmptyPropertyName is not null)
+        {
+            var nullOrEmptyPropertyInfo = AssociatedObject.GetType().GetProperty(
+                IfPropertyIsNullOrEmptyPropertyName,
+                BindingFlags.Instance | BindingFlags.Public
+                                      | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+
+            var value = nullOrEmptyPropertyInfo?.GetValue(AssociatedObject);
+            if (value is not string stringValue || !string.IsNullOrEmpty(stringValue))
+            {
+                return;
+            }
+        }
+        
+        if (IfPropertyIsNotNullOrEmptyPropertyName is not null)
+        {
+            var notNullOrEmptyPropertyInfo = AssociatedObject.GetType().GetProperty(
+                IfPropertyIsNotNullOrEmptyPropertyName,
+                BindingFlags.Instance | BindingFlags.Public
+                                      | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+
+            var value = notNullOrEmptyPropertyInfo?.GetValue(AssociatedObject);
+            if (value is not string stringValue || string.IsNullOrEmpty(stringValue))
+            {
+                return;
+            }
+        }
+        
         var propertyInfo = TargetObject.GetType().GetProperty(
             PropertyName,
             BindingFlags.Instance | BindingFlags.Public
